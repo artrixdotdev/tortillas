98742736fbc31b58c1259d2f320c9ddb0e1c0723
Artrix9095 <sefu786@outlook.com>
2025-03-10 11:40:16 -0700

    feat: Serialize pieces bytes into struct

--- lib/src/parser/mod.rs
+++ lib/src/parser/mod.rs
@@ -1,4 +1,7 @@
-use serde::{Deserialize, Serialize};
+use serde::{
+   Deserialize, Serialize,
+   de::{self, Visitor},
+};
 mod file;
 
 pub use file::*;
@@ -33,13 +36,51 @@ pub struct Info {
    #[serde(rename = "piece length")]
    piece_length: u64,
    /// Binary string of concatenated 20-byte SHA-1 hash values
-   /// TODO: Convert this to a custom type
-   pieces: String,
+   pieces: Hashes,
    // For single-file torrents, length may be here instead of in files
    #[serde(skip_serializing_if = "Option::is_none")]
    length: Option<u64>,
 }
 
+/// A custom type for serializing and deserializing a vector of 20-byte SHA-1 hashes.
+/// Credit to [Jon Gjengset](https://github.com/jonhoo/codecrafters-bittorrent-rust/)
+#[derive(Debug, Serialize)]
+pub struct Hashes(pub Vec<[u8; 20]>);
+
+// Add this implementation
+impl<'de> Deserialize<'de> for Hashes {
+   fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+   where
+      D: de::Deserializer<'de>,
+   {
+      deserializer.deserialize_bytes(HashesVisitor)
+   }
+}
+
+struct HashesVisitor;
+
+impl<'de> Visitor<'de> for HashesVisitor {
+   type Value = Hashes;
+
+   fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+      formatter.write_str("a byte string whose length is a multiple of 20")
+   }
+
+   fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+   where
+      E: de::Error,
+   {
+      if v.len() % 20 != 0 {
+         return Err(E::custom(format!("length is {}", v.len())));
+      }
+
+      Ok(Hashes(
+         v.chunks(20)
+            .map(|slice_20| slice_20.try_into().expect("guaranteed to be length 20"))
+            .collect(),
+      ))
+   }
+}
 #[derive(Debug, Serialize, Deserialize)]
 pub struct InfoFile {
    length: u64,
