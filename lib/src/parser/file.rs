d74f145387afa257112081ad2675a45e812a7b19
Artrix9095 <sefu786@outlook.com>
2025-03-10 14:38:25 -0700

    fix: Migrate file parser to MetaInfo enum

--- lib/src/parser/file.rs
+++ lib/src/parser/file.rs
@@ -1,14 +1,14 @@
-use super::Metainfo;
+use super::MetaInfo;
 use anyhow::Result;
 use serde_bencode as bencode;
 use std::path::PathBuf;
 
 use tokio::fs;
 
-/// Parse torrent file into [`Metainfo`](super::Metainfo).
-pub async fn parse_file(path: PathBuf) -> Result<Metainfo> {
+/// Parse torrent file into [`Metainfo`](super::MetaInfo).
+pub async fn parse_file(path: PathBuf) -> Result<MetaInfo> {
    let file = fs::read(path).await?;
-   let metainfo: Metainfo = bencode::from_bytes(&file)?;
+   let metainfo: MetaInfo = MetaInfo::Torrent(bencode::from_bytes(&file)?);
    Ok(metainfo)
 }
 
@@ -18,12 +18,17 @@ mod tests {
 
    #[tokio::test]
    async fn test_parse_file() {
-      println!("{}", std::env::current_dir().unwrap().display());
       let path = std::env::current_dir()
          .unwrap()
          .join("tests/torrents/big-buck-bunny.torrent");
+
       let metainfo = parse_file(path).await.unwrap();
 
-      assert_eq!(metainfo.info.name, "Big Buck Bunny");
+      match metainfo {
+         MetaInfo::Torrent(torrent) => {
+            assert_eq!(torrent.info.name, "Big Buck Bunny");
+         }
+         _ => panic!("Expected Torrent"),
+      }
    }
 }
