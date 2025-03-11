3c96cedd6903dbc07287adcc0a3e807a5e25ef9a
kurealnum <oscar.gaske.cs@gmail.com>
2025-03-11 16:25:03 -0400

    refactor: Moved AnnounceUri to enum, added custom Deserializer

--- lib/src/parser/mod.rs
+++ lib/src/parser/mod.rs
@@ -1,4 +1,9 @@
-use serde::Deserialize;
+use std::fmt;
+
+use serde::{
+   de::{self, Visitor},
+   Deserialize,
+};
 
 mod file;
 mod magnet;
@@ -9,8 +14,43 @@ pub use magnet::*;
 /// An Announce URI from a torrent file or magnet URI.
 /// https://www.bittorrent.org/beps/bep_0012.html
 /// Example: udp://tracker.opentrackr.org:1337/announce
-#[derive(Debug, Deserialize)]
-pub struct AnnounceUri(pub String);
+#[derive(Debug)]
+pub enum AnnounceUri {
+   Http(String),
+   Udp(String),
+   Websocket(String),
+}
+
+struct AnnounceUriVisitor;
+
+impl<'de> Deserialize<'de> for AnnounceUri {
+   fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+   where
+      D: serde::Deserializer<'de>,
+   {
+      deserializer.deserialize_string(AnnounceUriVisitor)
+   }
+}
+
+impl Visitor<'_> for AnnounceUriVisitor {
+   type Value = AnnounceUri;
+
+   fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+      formatter.write_str("a string")
+   }
+
+   fn visit_string<E>(self, s: String) -> Result<Self::Value, E>
+   where
+      E: de::Error,
+   {
+      Ok(match s.split("://").collect::<Vec<&str>>()[0] {
+         "http" => AnnounceUri::Http(s),
+         "udp" => AnnounceUri::Udp(s),
+         "wss" => AnnounceUri::Websocket(s),
+         _ => panic!(),
+      })
+   }
+}
 
 /// Always utilize MetaInfo instead of directly using TorrentFile or MagnetUri
 #[derive(Debug, Deserialize)]
