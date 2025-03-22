a02327e383f7f0abdc0d1d5145d26c833351e4d2
Artrix9095 <sefu786@outlook.com>
2025-03-22 10:16:08 -0700

    feat: Add Transport trait and ConnectedPeer implementation

--- lib/src/peers/mod.rs
+++ lib/src/peers/mod.rs
@@ -1,13 +1,14 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use serde::Serialize;
 use std::{
    fmt::Display,
    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
    sync::Arc,
 };
+use tokio::time::{Duration, Instant};
 
-use serde::Serialize;
-use tokio::time::Instant;
-
-use crate::hashes::InfoHash;
+use crate::hashes::{Hash, InfoHash};
 pub mod utp;
 
 /// Represents a BitTorrent peer with connection state and statistics
@@ -24,14 +25,70 @@ pub struct Peer {
    pub pieces: Vec<bool>,
    pub last_optimistic_unchoke: Option<Instant>,
    pub info_hash: Option<Arc<InfoHash>>,
-   pub peer_id: Option<[u8; 20]>,
+   pub id: Option<Hash<20>>,
    pub last_seen: Instant,
    pub bytes_downloaded: u64,
    pub bytes_uploaded: u64,
 }
+
 impl Display for Peer {
    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
       write!(f, "{}:{}", self.ip, self.port)
+   }
+}
+
+#[async_trait] // Async in traits are typically not allowed so we need this crate to make it work
+pub trait Transport: Send + Sync {
+   async fn connect(&mut self) -> Result<()>;
+   /// Should return the remote peer's ID
+   async fn handshake(&mut self, info_hash: Arc<InfoHash>, peer_id: Hash<20>) -> Result<Hash<20>>;
+
+   async fn send(&mut self, message: &PeerMessages) -> Result<()>;
+
+   async fn recv(&mut self, timeout: Option<Duration>) -> Result<PeerMessages>;
+
+   async fn close(&mut self) -> Result<()>;
+
+   fn is_connected(&self) -> bool;
+}
+/// A peer with a specific transport implementation.
+/// The point of this struct is to provide a way to interact with a peer using a specific transport implementation.
+pub struct ConnectedPeer<T: Transport> {
+   pub peer: Peer,
+   transport: T,
+}
+
+impl<T: Transport> ConnectedPeer<T> {
+   pub fn new(peer: Peer, transport: T) -> Self {
+      ConnectedPeer { peer, transport }
+   }
+
+   pub async fn connect(&mut self) -> Result<()> {
+      self.transport.connect().await
+   }
+
+   pub async fn handshake(
+      &mut self,
+      info_hash: Arc<InfoHash>,
+      peer_id: Hash<20>,
+   ) -> Result<Hash<20>> {
+      let remote_id = self.transport.handshake(info_hash.clone(), peer_id).await?;
+      self.peer.info_hash = Some(info_hash);
+      self.peer.id = Some(remote_id);
+
+      Ok(remote_id)
+   }
+
+   pub async fn send(&mut self, message: &PeerMessages) -> Result<()> {
+      self.transport.send(message).await
+   }
+
+   pub async fn recv(&mut self, timeout: Option<Duration>) -> Result<PeerMessages> {
+      self.transport.recv(timeout).await
+   }
+
+   pub async fn close(&mut self) -> Result<()> {
+      self.transport.close().await
    }
 }
 
@@ -87,7 +144,7 @@ impl Peer {
          pieces: vec![],
          last_optimistic_unchoke: None,
          info_hash: None,
-         peer_id: None,
+         id: None,
          last_seen: Instant::now(),
          bytes_downloaded: 0,
          bytes_uploaded: 0,
