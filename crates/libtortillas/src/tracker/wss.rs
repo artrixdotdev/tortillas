9c7d3aa7ea9c44aef6286153798ea3306ad46049
kurealnum <oscar.gaske.cs@gmail.com>
2025-05-07 19:35:07 -0400

    feat: Finished (but not tested) get_peers for WssTracker

--- crates/libtortillas/src/tracker/wss.rs
+++ crates/libtortillas/src/tracker/wss.rs
@@ -1,9 +1,12 @@
-use std::net::SocketAddr;
+use std::net::{IpAddr, SocketAddr};
+use std::str::FromStr;
 
 use anyhow::Result;
 use async_trait::async_trait;
-use futures_util::stream::{SplitSink, SplitStream};
-use futures_util::StreamExt;
+use futures::stream::StreamExt;
+use futures::stream::{SplitSink, SplitStream, TryStreamExt};
+use futures::SinkExt;
+use serde_json::Value;
 use tokio::net::TcpStream;
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::Message;
@@ -56,5 +59,52 @@ impl WssTracker {
 
 #[async_trait]
 impl TrackerTrait for WssTracker {
-   async fn get_peers(&mut self) -> Result<Vec<Peer>> {}
+   /// It should be noted that WebSockets are supposed to communicate in JSON. (This makes our
+   /// lives very easy though)
+   async fn get_peers(&mut self) -> Result<Vec<Peer>> {
+      trace!("Generated request parameters");
+      let mut tracker_request_as_json = serde_json::to_string(&self.params).unwrap();
+
+      // {tracker_request_as_json,info_hash:"xyz",peer_id:"abc"}
+      tracker_request_as_json.pop();
+      let request = format!(
+         "{},\"info_hash\":\"{}\",\"peer_id\":\"{}\"}}",
+         tracker_request_as_json, self.info_hash, self.peer_id
+      );
+
+      trace!("Request json generated: {}", request);
+      let message = Message::text(tracker_request_as_json);
+
+      trace!("Sending message to tracker");
+      self.write.send(message).await;
+
+      trace!("Recieving message from tracker");
+
+      // This section of code is completely and utterly scuffed. self.read.collect() refuses to
+      // work, so this is what we're stuck with for now.
+      let mut output = "".to_string();
+      while let Some(message) = self.read.next().await {
+         let data = message.unwrap().into_text().unwrap().to_string();
+         output += &data;
+      }
+
+      // Output should be a vec of peers
+      let res_json = serde_json::from_str(&output);
+      match res_json.unwrap() {
+         Value::Array(arr) => {
+            let mut res = vec![];
+            for i in 0..(arr.len()) {
+               let ip = IpAddr::from_str(arr[i]["ip"].as_str().unwrap()).unwrap();
+
+               let port = arr[i]["port"].as_u64().unwrap();
+               let peer = Peer::new(ip, port.try_into().unwrap());
+               res.push(peer);
+            }
+            Ok(res)
+         }
+         _ => {
+            panic!("Something went wrong with the peer response");
+         }
+      }
+   }
 }
