40bcfcb63ca5d2f6d153eb6ed534a3bba53c440f
kurealnum <oscar.gaske.cs@gmail.com>
2025-05-25 11:07:11 -0400

    refactor: Added tracing & handled get_all_peers

--- crates/libtortillas/src/engine/mod.rs
+++ crates/libtortillas/src/engine/mod.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use anyhow::{Ok, Result};
 use rand::random_range;
 use tokio::sync::{mpsc, Mutex};
-use tracing::error;
+use tracing::{error, trace};
 
 use crate::{
    hashes::InfoHash,
@@ -35,12 +35,17 @@ impl TorrentEngine {
    /// torrent file. The definition of a "valid file path" depends on where you're running the
    /// program from.
    async fn parse_input(&self, input: String) -> MetaInfo {
+      // Is this entirely accurate? Hopefully.
       match &input[0..8] {
          // Is a Magnet URI
-         "magnet:" => MagnetUri::parse(input).await.unwrap(),
+         "magnet:" => {
+            trace!("Input was Magnet URI");
+            MagnetUri::parse(input).await.unwrap()
+         }
 
          // Is a torrent file
          _ => {
+            trace!("Input was Torrent file");
             let path = std::env::current_dir().unwrap().join(input);
 
             TorrentFile::parse(path).await.unwrap()
@@ -53,13 +58,26 @@ impl TorrentEngine {
    fn get_announce_list(&self) -> Vec<Tracker> {
       // Is cloning the announce_list efficient? Probably not. But hey, it's Rust.
       match &self.metainfo {
-         MetaInfo::MagnetUri(magnet) => magnet.announce_list.clone().unwrap(),
+         MetaInfo::MagnetUri(magnet) => {
+            let announce_list = magnet.announce_list.clone().unwrap();
+            trace!(
+               "Succesfully cloned and unwrapped announce_list: {:?}",
+               announce_list
+            );
+            announce_list
+         }
          MetaInfo::Torrent(file) => {
             // Vec of vecs
             let flattened = file.announce_list.clone().unwrap().into_iter().flatten();
 
             // No longer a vec of vecs
-            flattened.collect()
+            let res = flattened.collect();
+
+            trace!(
+               "Succesfully cloned, unwrapped, and flattened announce_list: {:?}",
+               res
+            );
+            res
          }
       }
    }
@@ -81,12 +99,15 @@ impl TorrentEngine {
       let (tx, rx) = mpsc::channel(100);
       // Get an rx for each tracker
       let mut rx_list = vec![];
+
+      trace!("Making initial requests to trackers");
       for tracker in announce_list.iter() {
          rx_list.push(tracker.stream_peers(self.get_info_hash()).await.unwrap());
       }
 
-      // Repeatedely gather data from each rx and update self.peers as new peers are added
+      // Repeatedly gather data from each rx and update self.peers as new peers are added
       // rx
+      trace!("Spawning task to handle output from initial requests");
       tokio::spawn(async move {
          loop {
             for rx in rx_list.iter_mut() {
@@ -111,6 +132,7 @@ impl TorrentEngine {
    /// The full torrenting process, summarized in a single function. As of 5/23/25, the return
    /// value of this function is temporary.
    pub async fn torrent(&mut self, input: String) -> Result<()> {
+      trace!("Parsing input to torrent()");
       self.metainfo = self.parse_input(input).await;
 
       let info_hash = self.get_info_hash();
@@ -118,13 +140,18 @@ impl TorrentEngine {
       let announce_list: Vec<Tracker> = self.get_announce_list();
       let port: u16 = random_range(1024..65535);
 
-      // TODO
       // Call get_all_peers
-      let rx = self.get_all_peers(&announce_list);
+      let mut rx = self.get_all_peers(&announce_list).await.unwrap();
+
+      // Get initial peers
+      trace!("Getting initial peers...");
+      while let Some(res) = rx.recv().await {
+         self.peers.lock().await.extend(res);
+      }
 
       // Handle edge cases (ex. no peers)
 
-      // Create single instantce of transport
+      // Create single instance of transport
 
       // Handshake with all given peers
 
