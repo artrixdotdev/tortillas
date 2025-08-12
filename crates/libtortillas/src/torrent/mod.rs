d1c8f656320abd72af2c3f9913a79f9d84978525
artrixdotdev <sefu786@outlook.com>
2025-08-12 00:03:16 -0700

    feat: Add peer handshake and actor creation functionality

--- crates/libtortillas/src/torrent/mod.rs
+++ crates/libtortillas/src/torrent/mod.rs
@@ -3,14 +3,20 @@ use std::{collections::HashMap, sync::Arc};
 
 // REMOVE SOON
 use bitvec::vec::BitVec;
-use kameo::{Actor, actor::ActorRef};
+use kameo::{
+   Actor,
+   actor::ActorRef,
+   prelude::{Context, Message},
+};
 use librqbit_utp::UtpSocketUdp;
+use tracing::{instrument, warn};
 
 use crate::{
    errors::TrackerError,
+   hashes::InfoHash,
    metainfo::{Info, MetaInfo},
    peer::{Peer, PeerId},
-   protocol::PeerActor,
+   protocol::{PeerActor, stream::PeerStream},
    tracker::{Tracker, TrackerActor, udp::UdpServer},
 };
 
@@ -38,18 +44,73 @@ impl Torrent {
          }
       }
    }
+
+   fn info_hash(&self) -> InfoHash {
+      if let Some(info) = &self.info_dict() {
+         info.hash().unwrap()
+      } else {
+         match &self.metainfo {
+            MetaInfo::Torrent(t) => t.info.hash().unwrap(),
+            MetaInfo::MagnetUri(m) => m.info_hash().unwrap(),
+         }
+      }
+   }
+
+   /// Utility function that will create the peer actor and add it to the
+   /// torrent.
+   ///
+   /// This function calls [Self::handshake_peer] if no stream is provided to
+   /// retrieve the peer id
+   #[instrument(skip(self, peer, stream), fields(peer = ?peer.socket_addr()))]
+   async fn append_peer(&mut self, peer: &mut Peer, stream: Option<PeerStream>) {
+      // Should pass the stream to PeerActor at some point
+      let mut id = peer.id;
+      let stream = match stream {
+         Some(stream) => stream,
+         None => {
+            if let Ok((peer_id, stream, reserved)) = self.handshake_peer(&peer).await {
+               id = Some(peer_id);
+               peer.reserved = reserved;
+               stream
+            } else {
+               warn!("Failed to handshake with peer... silently existing");
+               return;
+            }
+         }
+      };
+      // Safe because we always know the id is defined by the lines above
+      let id = id.unwrap();
+
+      // Dont add ourselves as peers
+      if id == self.peer_id {
+         return;
+      }
+
+      let actor = PeerActor::spawn(PeerActor);
+      self.peers.insert(id, actor);
+   }
+
+   async fn handshake_peer(&self, peer: &Peer) -> anyhow::Result<(PeerId, PeerStream, [u8; 8])> {
+      let mut stream = PeerStream::connect(peer.socket_addr(), Some(self.utp_server.clone())).await;
+
+      let (id, reserved) = stream
+         .send_handshake(self.peer_id, Arc::new(self.info_hash()))
+         .await?;
+
+      Ok((id, stream, reserved))
+   }
 }
 
 pub(crate) enum TorrentMessage {
-   /// A message from an announce containing new Peers
+   /// A message from an announce actor containing new Peers
    Announce(Vec<Peer>),
 
    /// Sent after an incoming peer initializes a handshake
    /// The handshake will be preverified and routed to this torrent instance.
    ///
-   /// We as the instance are expected to reply, this is not the responsibility
-   /// of the engine.
-   IncomingPeer(Peer),
+   /// We as the instance are expected to reply to said handshake, this is not
+   /// the responsibility of the engine.
+   IncomingPeer(Peer, PeerStream),
 }
 
 impl Actor for Torrent {
@@ -86,5 +147,24 @@ impl Actor for Torrent {
          metainfo,
          info,
       })
+   }
+}
+impl Message<TorrentMessage> for Torrent {
+   type Reply = Option<()>;
+
+   async fn handle(
+      &mut self, message: TorrentMessage, ctx: &mut Context<Self, Self::Reply>,
+   ) -> Self::Reply {
+      match message {
+         TorrentMessage::Announce(peers) => {
+            for mut peer in peers {
+               self.append_peer(&mut peer, None).await;
+            }
+         }
+         TorrentMessage::IncomingPeer(mut peer, stream) => {
+            self.append_peer(&mut peer, Some(stream)).await
+         }
+      }
+      None
    }
 }
