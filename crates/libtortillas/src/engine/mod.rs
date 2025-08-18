e8c53ec2e846373dd9196e36d6c1fb0176ba8a58
artrixdotdev <sefu786@outlook.com>
2025-08-17 20:06:13 -0700

    feat: Add UDP server field to allow spawning Torrent actors
    Co-authored-by: kurealnum <oscar.gaske.cs@gmail.com>

--- crates/libtortillas/src/engine/mod.rs
+++ crates/libtortillas/src/engine/mod.rs
@@ -7,7 +7,7 @@ use tokio::net::TcpListener;
 
 use crate::{
    actor_request_response, errors::EngineError, hashes::InfoHash, metainfo::MetaInfo, peer::PeerId,
-   torrent::Torrent,
+   torrent::Torrent, tracker::udp::UdpServer,
 };
 
 pub(crate) enum EngineMessage {
@@ -22,31 +22,35 @@ actor_request_response!(
 pub struct Engine {
    tcp_socket: TcpListener,
    utp_socket: Arc<UtpSocketUdp>,
+   udp_server: UdpServer,
    torrents: Arc<DashMap<InfoHash, ActorRef<Torrent>>>,
    peer_id: PeerId,
 }
 
 impl Actor for Engine {
-   /// TCP socket address, uTP socket address
-   type Args = (Option<SocketAddr>, Option<SocketAddr>);
+   /// TCP socket address for incoming peers, uTP socket address for incoming
+   /// peers, UDP socket address for UDP trackers
+   type Args = (Option<SocketAddr>, Option<SocketAddr>, Option<SocketAddr>);
    type Error = EngineError;
 
    async fn on_start(
       args: Self::Args, actor_ref: kameo::prelude::ActorRef<Self>,
    ) -> Result<Self, Self::Error> {
-      let (tcp_addr, utp_addr) = args;
+      let (tcp_addr, utp_addr, udp_addr) = args;
       let tcp_addr = tcp_addr.unwrap_or(SocketAddr::from_str("0.0.0.0:0").unwrap());
-
       // Should this be port 6881?
       let utp_addr = utp_addr.unwrap_or(SocketAddr::from_str("0.0.0.0:0").unwrap());
+      let udp_addr = udp_addr.unwrap_or(SocketAddr::from_str("0.0.0.0:0").unwrap());
 
       let tcp_socket = TcpListener::bind(tcp_addr).await.unwrap();
       let utp_socket = UtpSocketUdp::new_udp(utp_addr).await.unwrap();
+      let udp_server = UdpServer::new(Some(udp_addr)).await;
 
       let peer_id = PeerId::new();
       Ok(Self {
          tcp_socket,
          utp_socket,
+         udp_server,
          torrents: Arc::new(DashMap::new()),
          peer_id,
       })
