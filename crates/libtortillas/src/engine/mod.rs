a8a733087d58a57ee8af35113cc8449bca9ef02a
artrixdotdev <sefu786@outlook.com>
2025-08-26 13:41:19 -0700

    feat: Create `new` and `actor` methods on `Engine` struct

--- crates/libtortillas/src/engine/mod.rs
+++ crates/libtortillas/src/engine/mod.rs
@@ -1,5 +1,18 @@
 mod actor;
 pub(crate) use actor::*;
-use kameo::actor::ActorRef;
+use kameo::{Actor, actor::ActorRef};
 
 pub struct Engine(ActorRef<EngineActor>);
+
+impl Engine {
+   pub fn new(addrs: Option<EngineActorArgs>) -> Self {
+      let addrs = addrs.unwrap_or_default();
+      let actor = EngineActor::spawn(addrs);
+      Engine(actor)
+   }
+   
+   /// Just a helper function so we don't have to write `&self.0` all the time.
+   fn actor(&self) -> &ActorRef<EngineActor> {
+      &self.0
+   }
+}
