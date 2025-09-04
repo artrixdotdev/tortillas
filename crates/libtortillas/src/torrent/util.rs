37ed6302728b1358654a89757b8ada636887706b
kurealnum <oscar.gaske.cs@gmail.com>
2025-09-04 16:48:49 -0400

    feat: Implemented piece validation

--- crates/libtortillas/src/torrent/util.rs
+++ crates/libtortillas/src/torrent/util.rs
@@ -1,10 +1,15 @@
 use std::{io::SeekFrom, path::Path};
 
+use anyhow::anyhow;
 use bytes::Bytes;
+use sha1::{Digest, Sha1};
 use tokio::{
    fs::{File, OpenOptions},
-   io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt, copy},
+   io::{self, AsyncReadExt, AsyncSeekExt, AsyncWriteExt, copy},
+   task::spawn_blocking,
 };
+
+use crate::hashes::{Hash, HashVec};
 
 pub async fn create_empty_file(path: impl AsRef<Path>, length: usize) -> anyhow::Result<()> {
    let mut out = File::create(path).await?;
@@ -41,6 +46,29 @@ pub async fn write_block_to_file(
    file.seek(SeekFrom::Start(offset as u64)).await?;
 
    file.write_all(&block).await?;
+
+   Ok(())
+}
+
+/// Validates a piece file given the path and the pieces hash from our info
+/// dictionary (the torrent's metadata). This function should only be called
+/// when we have the info dictionary from either the orginal `.torrent` file or
+/// from retrieving the info dict via BEP 0010/BEP 0009.
+pub async fn validate_piece_file(
+   path: impl AsRef<Path> + Send + 'static, hash: Hash<20>,
+) -> anyhow::Result<()> {
+   let piece_file_hash = spawn_blocking(move || {
+      let mut hasher = Sha1::new();
+      let mut file = std::fs::File::open(&path).unwrap();
+      std::io::copy(&mut file, &mut hasher).unwrap();
+      let hash = hasher.finalize();
+      Hash::from_bytes(hash.into())
+   })
+   .await?;
+
+   if piece_file_hash != hash {
+      return Err(anyhow!("Hashed file was not equal to hash from info dict"));
+   }
 
    Ok(())
 }
