99f3f5a4e6312c97fe14d9427b1b5a7739c5c434
kurealnum <oscar.gaske.cs@gmail.com>
2025-09-05 08:30:44 -0400

    docs: Added documentation for all functions in util

--- crates/libtortillas/src/torrent/util.rs
+++ crates/libtortillas/src/torrent/util.rs
@@ -5,12 +5,22 @@ use bytes::Bytes;
 use sha1::{Digest, Sha1};
 use tokio::{
    fs::{File, OpenOptions},
-   io::{self, AsyncReadExt, AsyncSeekExt, AsyncWriteExt, copy},
+   io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt, copy},
    task::spawn_blocking,
 };
 
-use crate::hashes::{Hash, HashVec};
+use crate::{hashes::Hash, protocol::messages::PeerMessages};
 
+/// Creates a blank file padded with 0's.
+///
+/// # Examples
+///
+/// ```no_run
+/// let path = "/tmp/my-file";
+/// util::create_empty_file(path, 100);
+///
+/// assert!(path.exists());
+/// ```
 pub async fn create_empty_file(path: impl AsRef<Path>, length: usize) -> anyhow::Result<()> {
    let mut out = File::create(path).await?;
    if cfg!(target_family = "unix") {
@@ -33,6 +43,19 @@ pub async fn create_empty_file(path: impl AsRef<Path>, length: usize) -> anyhow:
    Ok(())
 }
 
+/// Writes a single block from a
+/// [Piece](crate::protocol::messages::PeerMessages::Piece) message to a file
+/// already padded with 0's. See [self::create_empty_file] for more.
+///
+/// # Examples
+/// ```no_run
+/// let message = PeerMessages::Piece(0, 0, Bytes::new());
+/// let path = "/tmp/my-file"
+///
+/// if let PeerMessages::Piece(index, begin, block) = message {
+///   util::write_block_to_file(path, begin, block);
+/// }
+/// ```
 pub async fn write_block_to_file(
    path: impl AsRef<Path>, offset: usize, block: Bytes,
 ) -> anyhow::Result<()> {
@@ -51,9 +74,20 @@ pub async fn write_block_to_file(
 }
 
 /// Validates a piece file given the path and the pieces hash from our info
-/// dictionary (the torrent's metadata). This function should only be called
-/// when we have the info dictionary from either the orginal `.torrent` file or
-/// from retrieving the info dict via BEP 0010/BEP 0009.
+/// dictionary (the torrent's metadata).
+///
+/// This function should only be called when we have the info dictionary from
+/// either the orginal `.torrent` file or from retrieving the info dict via [BEP 0010](https://www.bittorrent.org/beps/bep_0010.html) or [BEP 0009](https://www.bittorrent.org/beps/bep_0009.html),
+/// and when we know that we've acquired every block for a piece.
+///
+/// # Examples
+/// ```no_run
+/// let info_dict = Info { ... };
+/// let path = "/tmp/my-file";
+/// let current_piece_hash = info_dict.pieces[0];
+///
+/// util::validate_piece_file(path, current_piece_hash);
+/// ```
 pub async fn validate_piece_file(
    path: impl AsRef<Path> + Send + 'static, hash: Hash<20>,
 ) -> anyhow::Result<()> {
